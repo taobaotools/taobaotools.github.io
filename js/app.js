53ba1b62926bf00fe10e35ca87019b95995be271
Will <william8ga+taobaotools@gmail.com>
2017-03-19 15:08:54 +1100

    JavaScript Rewrite

--- js/app.js
+++ js/app.js
@@ -1,67 +1,104 @@
 /**
- * The JavaScript for everything :)
+ * Helper class that does all the calculations and converting. All UI element
+ * interactions are handled in 'preparer.js'
  */
-function convert() {
-    // Create variables to store data
-    var links = document.getElementById('content').value.split('\n');
+const ITEM_ID = 'id=';
+const ITEM = 'item/';
+const SHOP_ID = 'shop_id=';
+const TAOBAO_URL = 'https://item.taobao.com/item.htm?id=';
+const SHOP_URL = 'https://shop{}.taobao.com';
+const M_INTL = 'm.intl.taobao.com';
+const H5 = 'h5.m.taobao.com';
+const WORLD = 'world.taobao.com';
+const SHOP_M = 'shop.m.taobao.com';
+
+/**
+ * Given an array of URLs, separate them into valid and invalid ones, convert them and return
+ */
+function convertURLs(urls) {
+    // Initialise our empty arrays
     var validLinks = [];
     var invalidLinks = [];
 
-    // Go through given links, convert and add to relevant array
-    for (var i = 0; i < links.length; i++) {
-        var current = links[i];
-        if (isTaobaoURL(current)) {
-            // Convert URL based on type and clean it up
-            validLinks.push(cleanURL(convertURL(current)));
-        } else {
-            invalidLinks.push(links[i]);
+    // Go through each element of the parameter array and convert as required
+    urls.forEach(function(url) {
+        if (isTaobaoURL(url))
+            validLinks.push(convertURL(url));
+        else
+            invalidLinks.push(url);
+    });
+
+    // Return object containing our valid links and invalid links array
+    return {
+        valid : validLinks,
+        invalid : invalidLinks
+    };
+}
+
+
+/**
+ * Given a valid, Taobao url we convert it into Chinese mainland form
+ */
+function convertURL(str) {
+    if (contains(str, M_INTL) || contains(str, H5)) {
+        // Normal international or H5 app link
+        return buildTaobaoURL(str, ITEM_ID, false)
+    } else if (contains(str, WORLD)) {
+        // world.taobao.com link
+        if (contains(str, 'item'))
+            // World item
+            return buildTaobaoURL(str, ITEM, false);
+        else
+            // World store
+            return str.replace('world.taobao.com', 'taobao.com');
+    } else if (contains(str, SHOP_M)) {
+        // Mobile shop with shop ID
+        return buildTaobaoURL(str, SHOP_ID, true);
+    } else if (!contains(str, 'item')) {
+        // Most mobile shops
+        return str.replace('m.taobao.com', 'taobao.com');
+    } else {
+        // Already valid Taobao URL, canonacalise it
+        return buildTaobaoURL(str, 'id=', false);
+    }
+}
+
+
+/**
+ * Given a string URL and a query we want to match, get the ID which is numeric only
+ */
+function getID(str, match) {
+    var start = str.indexOf(match) + match.length;
+    var end = str.length;
+    for (var i = start; i < str.length; i++) {
+        if (!isDigit(str.charAt(i))) {
+            end = i;
+            break;
         }
     }
+    return str.substring(start, end);
+}
 
-    // Change status message
-    var invalidLength;
-    if (invalidLinks.length == 1 && invalidLinks[0] == '') {
-        invalidLength = 0;
+
+/**
+ * Given a string, and query we want to match, we find, replace and return
+ */
+function buildTaobaoURL(str, match, isShop) {
+    if (isShop) {
+        var shopID = getID(str, match);
+        return SHOP_URL.replace('{}', shopID);
     } else {
-        invalidLength = invalidLinks.length;
-    }
-    setHTML('message', getStatus(links.length, validLinks.length, invalidLength));
-
-    // Build valid links
-    if (validLinks.length > 0)  {
-        setComponents(true, validLinks);
-    }
-
-    // Build invalid links
-    if (invalidLinks.length > 0 && invalidLinks[0] != '') {
-        setComponents(false, invalidLinks)
+        var itemID = getID(str, match);
+        return TAOBAO_URL + itemID;
     }
 }
 
-/* Prepare components to show valid and invalid links */
-function setComponents(isValid, array) {
-    // Set display visibility and change text within buttons
-    var id = isValid ? 'valid' : 'invalid';
-    setDisplay('content', false);
-    setDisplay(id, true);
-    setDisplay(id + '-copy', true);
-    setHTML(id + '-copy', 'Copy to clipboard');
 
-    // Build HTML
-    var html = '<h3>' + (isValid ? 'Valid' : 'Invalid') + '</h3>';
-    for (var i = 0; i < array.length; i++) {
-        html += '<a class=\'link\' href=\'' + array[i] + '\' target=\'_blank\'>' + array[i] + '</a>'
-    }
-
-    // Set components
-    setHTML(id, html);
-    setHTML(id + '-hidden', array.join('<br>')); // this is bad
-    document.getElementById('actions').style.paddingTop = '15px';
-}
-
-
-/* Checks if string is valid URL and that it contains 'taobao.com' */
+/**
+ * Given a string, we check and return if it is a valid Taobao URL or not
+ */
 function isTaobaoURL(str) {
+    // Define our Regex pattern and test the string
     var urlPattern = new RegExp('^(https?:\\/\\/)?'+
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+
         '((\\d{1,3}\\.){3}\\d{1,3}))'+
@@ -72,145 +109,18 @@ function isTaobaoURL(str) {
 }
 
 
-/* Converts the Taobao URL */
-function convertURL(str) {
-    if (str.indexOf('m.intl.taobao.com') != -1) {
-        // General mobile item
-        return str.replace('m.intl', 'item').replace('detail/detail', 'item').replace('detail/desc', 'item');
-    } else if (str.indexOf('h5.m.taobao.com') != -1) {
-        // Mobile app item, substring to get item ID and build new URL
-        var start = str.indexOf('?id=') + 4;
-         // Default end just length of url, loop to find alternatives
-        var end = str.length;
-        for (var i = start; i < str.length; i++) {
-            if (!isDigit(str.charAt(i))) {
-                end = i;
-                break;
-            }
-        }
-        var item_id = str.substring(start, end);
-        return "https://item.taobao.com/item.htm?id=" + item_id;
-    } else if (str.indexOf('m.taobao.com') != -1) {
-        if (str.indexOf('shop.m.taobao.com') != -1) {
-            // Weird mobile stores - e.g. shop.m.taobao.com/shop/....shop_id
-            var start = str.indexOf('shop_id=') + 8;
-            // Default end is length of URL
-            var end = str.length;
-            // Go through rest of URL, stop when we find non-digit
-            for (var i = start; i < str.length; i++) {
-                if (!isDigit(str.charAt(i))) {
-                    end = i;
-                    break;
-                }
-            }
-            var shopID = str.substring(start, end);
-            return 'https://shop' + shopID + '.taobao.com';
-        } else if (str.indexOf('item') == -1) {
-            // Most mobile stores
-            return str.replace('m.taobao', 'taobao');
-        } else {
-            // Already valid Taobao URL
-            var start = str.indexOf('id=') + 3;
-            // Default end is length of URL
-            var end = str.length;
-            // Go through rest of URL, stop when we find non-digit
-            for (var i = start; i < str.length; i++) {
-                if (!isDigit(str.charAt(i))) {
-                    end = i;
-                    break;
-                }
-            }
-            var id = str.substring(start, end);
-            return 'https://item.taobao.com/item.htm?id=' + id;
-        }
-    } else if (str.indexOf('world.taobao.com') != -1) {
-        if (str.indexOf('item') != -1) {
-            // World item, we substring to get the item ID and then build the new URL
-            var start = str.indexOf('item/') + 5;
-            var end = str.indexOf('.htm');
-            var itemID = str.substring(start, end);
-            return 'https://item.taobao.com/item.htm?id=' + itemID;
-        } else {
-            // General world store
-            return str.replace('world.taobao', 'taobao');
-        }
-    } else {
-        // Already valid Taobao URL
-        var start = str.indexOf('id=') + 3;
-        // Default end is length of URL
-        var end = str.length;
-        // Go through rest of URL, stop when we find non-digit
-        for (var i = start; i < str.length; i++) {
-            if (!isDigit(str.charAt(i))) {
-                end = i;
-                break;
-            }
-        }
-        var id = str.substring(start, end);
-        return 'https://item.taobao.com/item.htm?id=' + id;
-    }
+/**
+ * Given a string and a query parameter, we check if the string contains that query
+ */
+function contains(str, query) {
+    return str.indexOf(query) != -1;
 }
 
 
-/* Cleans up a URL - FIXME THIS IS REALLY BAD! */
-function cleanURL(str) {
-    var builder = '';
-    var encounteredQuestion = false;
-    for (var j = 0; j < str.length; j++) {
-        if (str.charAt(j) == '?' && encounteredQuestion) {
-            builder += '&';
-        } else if (str.charAt(j) == '?') {
-            builder += '?';
-            encounteredQuestion = true;
-        } else {
-            builder += str.charAt(j);
-        }
-    }
-    return builder;
-}
-
-
-/* Change inner html of a DOM element */
-function setHTML(elementName, html) {
-    document.getElementById(elementName).innerHTML = html;
-}
-
-
-/* Set display of html of a DOM element */
-function setDisplay(elementName, visible) {
-    document.getElementById(elementName).style.display = (visible ? 'unset' : 'none');
-}
-
-
-/* Builds the status message based on the counts */
-function getStatus(total, valid, invalid) {
-    if (invalid == total) {
-        return 'All the links are invalid.';
-    } else {
-        return 'Converted ' + valid + linkOrLinks(valid) + ' Found ' + invalid + ' invalid' + linkOrLinks(invalid);
-    }
-}
-
-
-/* English Grammar */
-function linkOrLinks(count) {
-    return count == 1 ? ' link.' : ' links.';
-}
-
-
-/* Reset HTML elements */
-function reset() {
-    document.getElementById('content').value = '';
-    setDisplay('content', true);
-    setHTML('message', '');
-    setDisplay('valid', false);
-    setDisplay('valid-copy', false);
-    setDisplay('invalid-copy', false);
-    setDisplay('invalid', false);
-}
-
-/* Check if char is digit */
+/**
+ * Given a character, check whether it is a digit or not by converting to ASCII code
+ */
 function isDigit(char) {
-    return char == '0' || char == '1' || char == '2' || char == '3' || char == '4' ||
-            char == '5' || char == '6' || char == '7' || char == '8' || char == '9';
+    char = char.charCodeAt(0);
+    return char >= 48 && char <= 57;
 }
---
+++ js/preparer.js
@@ -0,0 +1,103 @@
+/**
+ * Main convert function that is triggered when we click 'Convert'
+ */
+function convert() {
+    // Convert the links in the text area and place in arrays
+    var links = convertURLs(document.getElementById('content').value.split('\n'));
+    var validLinks = links.valid;
+    var invalidLinks = cleanArray(links.invalid);
+
+    // Set Message for user
+    setHTML('message', getStatus(validLinks.length, invalidLinks.length));
+
+    // Build valid and invalid links
+    setComponents(true, validLinks);
+    setComponents(false, invalidLinks);
+}
+
+
+/**
+ * Change inner html of a DOM element
+ */
+function setHTML(elementName, html) {
+    document.getElementById(elementName).innerHTML = html;
+}
+
+
+/**
+ * Set display of html of a DOM element
+ */
+function setDisplay(elementName, visible) {
+    document.getElementById(elementName).style.display = (visible ? 'unset' : 'none');
+}
+
+
+/**
+ * Builds the status message at our given states based on the length of the arrays
+ */
+function getStatus(validLength, invalidLength) {
+    if (validLength + invalidLength == 0) {
+        return 'All the links are invalid.';
+    } else {
+        return 'Converted ' + validLength + linkOrLinks(validLength)
+            + ' Found ' + invalidLength + ' invalid' + linkOrLinks(invalidLength);
+    }
+}
+
+
+/**
+ *  English Grammar to find whether we have a link or links
+ */
+function linkOrLinks(count) {
+    return count == 1 ? ' link.' : ' links.';
+}
+
+
+/**
+ * Clean up an array by removing all the empty string
+ */
+function cleanArray(array) {
+    var newArray = [];
+    array.forEach(function(elem) {
+        if (elem != '')
+            newArray.push(elem);
+    });
+    return newArray;
+}
+
+
+/* Prepare components to show valid and invalid links */
+function setComponents(isValid, array) {
+    // Do not process empty array of lists
+    if (array.length == 0)
+        return;
+
+    // Set display of text area to false and show the built links
+    var id = isValid ? 'valid' : 'invalid';
+    setDisplay('content', false);
+    setDisplay(id, true);
+
+    // Build HTML
+    var html = '';
+    array.forEach(function(link) {
+        html += '<a href=\'' + link + '\' target=\'_blank\'>' + link + '</a><br>'
+    });
+
+    // Set components
+    setHTML(id + '-content', html);
+    setHTML(id + '-hidden', html);
+}
+
+
+/**
+ * Reset HTML elements on the web page
+ */
+function reset() {
+    document.getElementById('content').value = '';
+    setDisplay('content', true);
+    setHTML('message', '');
+    setDisplay('valid', false);
+    setDisplay('invalid', false);
+    setHTML('valid-copy', 'Copy to clipboard');
+    setHTML('invalid-copy', 'Copy to clipboard');
+}
